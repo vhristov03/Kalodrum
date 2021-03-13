a4e318eeb5d0137d5f66ec7cebb4018004910746
Kalin Asenov <72384461+Blepeian@users.noreply.github.com>
2021-03-13 12:03:46 +0200

    Pesho can double jump

--- PeshoProgramista/PeshoProgramista.gd
+++ PeshoProgramista/PeshoProgramista.gd
@@ -1,38 +1,37 @@
 extends KinematicBody2D
 
-export (int) var speed = 10000
-var velocity = Vector2()
+export (int) var speed = 300
+export (int) var jump_speed = -500
+export (int) var gravity = 1500
 
+var velocity = Vector2()
+var is_jumping = false
+var can_double_jump = false
 
 func _ready():
 	pass
 
-
-func get_input(delta):
-	velocity = Vector2()
-	velocity.y += 1
-
-	if Input.is_action_pressed('move_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('move_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed("jump"):
-		velocity.y -= 2
-		speed += 200
-	velocity = velocity.normalized() * speed * delta
+func get_input():
+	velocity.x = 0
+	
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += speed
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= speed
+	if Input.is_action_pressed('ui_up') and is_on_floor():
+		velocity.y = jump_speed
+		is_jumping = true
+		can_double_jump = true
 
 
 func _physics_process(delta):
-	get_input(delta)
-	velocity = move_and_slide(velocity)
+	get_input()
+	velocity.y += gravity * delta
+	if is_jumping and is_on_floor():
+		is_jumping = false
+	if !is_on_floor() and can_double_jump and Input.is_action_just_pressed('ui_up'):
+		velocity.y = jump_speed
+		can_double_jump = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
 
 
-func _on_Area2D_area_entered(area):
-	pass # Replace with function body.
-
-
-func _on_Area2D_area_exited(area):
-	if "platform" in area.get_name():
-		Input.action_release("collide")
-		print("No longer colliding")
-	pass # Replace with function body.
