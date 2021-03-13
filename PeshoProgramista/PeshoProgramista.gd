7bc7aa203c118a6b7cf13a0a2f69dbf18b033a59
z5kb <78223226+z5kb@users.noreply.github.com>
2021-03-13 10:41:47 +0200

    fix collision to work on a tileset, remove stuff related to collision in pesho script, remove platforms

--- PeshoProgramista/PeshoProgramista.gd
+++ PeshoProgramista/PeshoProgramista.gd
@@ -7,38 +7,28 @@ var velocity = Vector2()
 func _ready():
 	pass
 
+
 func get_input(delta):
 	velocity = Vector2()
 	velocity.y += 1
-	if Input.is_action_pressed("collide"):
-		velocity.y -= 1
+
 	if Input.is_action_pressed('move_right'):
 		velocity.x += 1
 	if Input.is_action_pressed('move_left'):
 		velocity.x -= 1
 	if Input.is_action_pressed("jump"):
 		velocity.y -= 2
+		speed += 200
 	velocity = velocity.normalized() * speed * delta
-	
-	
-	
-	
-	
+
 
 func _physics_process(delta):
 	get_input(delta)
 	velocity = move_and_slide(velocity)
-	
-
 
 
 func _on_Area2D_area_entered(area):
-	if "platform" in area.get_name():
-		Input.action_press("collide")
-		print("collision")
 	pass # Replace with function body.
-	
-
 
 
 func _on_Area2D_area_exited(area):
